9935768a9d910a2dda3748781189a757620b08d0
NicoleKelbey <nikki.markert@yahoo.com>
2024-10-31 10:22:53 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
-<div>
-  
-</div>
+import { puppyList } from './data'
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppies);
+  return (
+    <>
+      <div>
+        {
+          puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
+      </div>
+
+    </>
+  )
+}
+
+export default App
